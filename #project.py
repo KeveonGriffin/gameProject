95a4c4dc542e0193e6adc47ecf878de93b883cc4
Keveon Griffin <griffin.keveon22@gmail.com>
2023-11-24 16:53:43 -0500

    created the floor of game (may appear buggy)

--- #project.py
+++ #project.py
@@ -6,4 +6,12 @@ GRID_SIZE = 50
 WIDTH = GRID_WIDTH * GRID_SIZE
 HEIGHT = GRID_HEIGHT * GRID_SIZE
 
+def screen_coords(x,y):
+    return(x * GRID_SIZE, y * GRID_SIZE)
+def draw_background():
+    for y in range(GRID_HEIGHT):
+        for x in range(GRID_HEIGHT):
+            screen.blit("floor2", screen_coords(x,y))
+def draw():
+    draw_background()
 pgzrun.go()
